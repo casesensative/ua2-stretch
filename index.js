2c24459e2eabef35a6f335b73fd8aef059fbf38d
casesensative <victor.waters1@gmail.com>
2021-04-18 15:08:56 -0400

    Finished Stretch Assignment (refresh?)

---
+++ index.js
@@ -0,0 +1,76 @@
+const formBtn  = document.getElementById('close-form');
+const form = document.querySelector('form');
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+
+const formContainer = document.getElementsByClassName('form-container');
+
+const emailList = [];
+
+const main = document.querySelector('main');
+const cart = document.createElement('div');
+
+let cartItems = 0;
+
+function addToCart() {
+  if (cartItems === 0) {
+    cartItems = 1;
+    cart.setAttribute('class', 'cart-display');
+    cart.innerText = ('Your Cart: 1 Item');
+    main.appendChild(cart);
+  } else {
+    cartItems++;
+    cart.innerText = (`Your Cart: ${cartItems} Item`); 
+  }
+}
+
+
+function toggler() {
+  form.classList.toggle('hide');
+  formBtn.innerText === 'X' ? formBtn.innerText = '+' : formBtn.innerText = 'X';
+}
+
+function validateForm() {
+  console.log(nameInput.value);
+  if (!nameInput.value && !emailInput.value) {
+    nameInput.style.cssText = 'border: 2px solid red;';
+    emailInput.style.cssText = 'border: 2px solid red;';
+    alert('You must enter a name and email address to subscribe!');
+    return false;
+  } else if (!nameInput.value && emailInput.value) {
+    nameInput.style.cssText = 'border: 2px solid red;'
+    alert('Please enter a name to subscribe!');
+    return false;
+  } else if (nameInput.value && !emailInput.value) {
+    emailInput.style.cssText = 'border: 2px solid red;';
+    alert('You must enter an email address to subscribe!');
+    return false;
+  } else {
+    emailList.push({name: nameInput, email: emailInput});
+    return true;
+  }
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault;
+  validateForm();
+  displayThankYou();
+});
+
+
+
+function displayThankYou() {
+  formContainer.innerText = 'Thank you for Subscribing!';
+  setTimeout(function () {
+    formContainer.remove();
+  }, 3000);
+}
+
+nameInput.addEventListener('change', function() {
+  nameInput.style.border = none;
+})
+
+emailInput.addEventListener('change', function() {
+  emailInput.style.border = none;
+})
